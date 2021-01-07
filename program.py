dddae8a999a6c065bf31aa0ffb63a83a389b3512
Stoeckchen <l.stocker1@gmx.de>
2021-01-07 10:08:28 +0100

    l

--- program.py
+++ program.py
@@ -7,6 +7,7 @@ import adafruit_bme680
 i2c = I2C(board.SCL, board.SDA)
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug = False)
 
+print("Uhrzeit: ", datetime.utcnow().isoformat())
 print("Temperatur: ", bme680.temperature, "°C")
 print("Gas: ",bme680.gas, "Ohm")
 print("Relative Luftfeuchtigkeit: ", bme680.relative_humidity, "%")
